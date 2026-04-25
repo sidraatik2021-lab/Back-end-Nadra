bf68598a585e95d68b85d047735befaab6ce635f
Nadhifa Hassan <nadhifabarakat@gmail.com>
2026-04-25 22:03:10 +0200

    fix

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 import express from "express";
-import skincareRoutes from "./routes/skincareRoutes.js";
-import mongoose from "mongoose";
-import "dotenv/config";
 import cors from "cors";
+import "dotenv/config";
+import skincareRoutes from "./routes/skincareRoutes.js";
 import offersRoutes from "./routes/offersRoutes.js";
 import laserRoutes from "./routes/laserRoutes.js";
-import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
+import connectDB from "./config/db.js";
+
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use("/skincare", skincareRoutes);
 app.use("/offers", offersRoutes);
 app.use("/laser", laserRoutes);
 app.use("/contact", contactRoutes);
+
 connectDB();
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("Connected to MongoDB"))
-//   .catch((err) => console.error("Error connecting to MongoDB:", err));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
